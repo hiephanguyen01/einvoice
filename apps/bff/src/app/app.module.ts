6176c7845c9a029781bd0965b81c92ce9b409e2f
hiepnh <hiepnh@vihatgroup.com>
2026-03-28 03:53:41 +0700

    feat(middlewares): add logger middleware and integrate with NestJS application
    - Updated .prettierrc for consistent code formatting.
    - Created LoggerMiddleware to log incoming requests.
    - Integrated LoggerMiddleware in AppModule.
    - Enhanced main.ts to include Swagger documentation and CORS support.
    - Added middlewares library with necessary configurations (eslint, jest, tsconfig).
    - Updated package.json and pnpm-lock.yaml to include new dependencies.
    - Modified tsconfig.base.json to include path for middlewares.

--- apps/bff/src/app/app.module.ts
+++ apps/bff/src/app/app.module.ts
@@ -1,16 +1,19 @@
-import { Module } from '@nestjs/common';
+import { LoggerMiddleware } from '@common/middlewares/logger.middleware';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { CONFIGURATION, IConfiguration } from './../configuration/index';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true, load: [() => CONFIGURATION] }),
-  ],
+  imports: [ConfigModule.forRoot({ isGlobal: true, load: [() => CONFIGURATION] })],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {
   static CONFIGURATION: IConfiguration = CONFIGURATION;
+
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
 }
--- apps/bff/src/main.ts
+++ apps/bff/src/main.ts
@@ -5,18 +5,44 @@
 
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = AppModule.CONFIGURATION.GLOBAL_PREFIX;
-  app.setGlobalPrefix(globalPrefix);
-  app.useGlobalPipes(new ValidationPipe({transform: true, whitelist: true}));
-  const port = AppModule.CONFIGURATION.APP_CONFIG.PORT;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
+  try {
+    const app = await NestFactory.create(AppModule);
+    const globalPrefix = AppModule.CONFIGURATION.GLOBAL_PREFIX;
+    app.setGlobalPrefix(globalPrefix);
+    app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+
+    app.enableCors({
+      origin: '*',
+    });
+
+    // Swagger Documentation
+    const config = new DocumentBuilder()
+      .setTitle('EInvoice-bff API')
+      .setDescription('The EInvoice-bff API description')
+      .setVersion('1.0.0')
+      .addBearerAuth({
+        description: 'Default JWT Authorization',
+        type: 'http',
+        in: 'header',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'Authorization',
+      })
+      .build();
+
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup(`${globalPrefix}/docs`, app, documentFactory);
+    const port = AppModule.CONFIGURATION.APP_CONFIG.PORT;
+    await app.listen(port);
+    Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
+    Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}/docs`);
+  } catch (error) {
+    Logger.error('Error starting the application', error);
+  }
 }
 
 bootstrap();
